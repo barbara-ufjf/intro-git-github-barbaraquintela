32b042a6c98525561fb5a3fc99778d014bafb338
Instituto de Ciencias Exatas <ice@lab05m51.nrc.ice.ufjf.br>
2023-03-23 15:09:35 -0300

    update files

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello world";
+    cout << "Hello world" << endl;
     return 0;
-<<<<<<< HEAD
-} 
-=======
 }
->>>>>>> 4e9af8469813d2b6a37b33a50584a634161e775e
